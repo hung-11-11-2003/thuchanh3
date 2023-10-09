2c4052b37028ce7157c707642cca26824cbfba6a
Trần Nguyễn Phi Hùng <2124802010539@student.tdmu.edu.vn>
2023-10-09 14:38:53 +0700

    first commit

---
+++ SachOnline/Controllers/GioHangController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SachOnline.Models;
+
+namespace SachOnline.Controllers
+{
+    public class GioHangController : Controller
+    {
+        dbSachOnlineDataContext dbSachOnlineDataContext;
+        // GET: GioHang
+        // GET: GioHang
+        public List<GioHang> LayGioHang()
+        {
+            List<GioHang> lstGioHang = Session["GioHang"] as List<GioHang>;
+            if (lstGioHang == null)
+            {
+                //Khởi tạo Giỏ hàng (giỏ hàng chưa tồn tại) 
+                lstGioHang = new List<GioHang>();
+                Session["GioHang"] = lstGioHang;
+            }
+            return lstGioHang;
+
+        }
+        public ActionResult ThemGioHang(int ms, string url) 
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            GioHang sp = lstGioHang.Find(n=>n.iSachID==ms);
+            if (sp == null)
+            {
+                sp = new GioHang(ms);
+                lstGioHang.Add(sp);
+            }
+            else
+            {
+                sp.iSoLuong++;
+            }
+            return Redirect(url);
+        }
+
+        private int TongSoLuong()
+        {
+            int iTongSoLuong = 0;
+            List<GioHang> lstGIoHang = Session["GioHang"] as List <GioHang>;
+            if(lstGIoHang != null)
+            {
+                iTongSoLuong = lstGIoHang.Sum(n=>n.iSoLuong);
+            }
+            return iTongSoLuong;
+        }
+
+        private double TongTien()
+        {
+            double dTongTien = 0;
+            List<GioHang> lstGioHang = Session["GioHang"] as List<GioHang>;
+            if (lstGioHang != null)
+            {
+                dTongTien = lstGioHang.Sum(n => n.dTongTien);
+            }    
+            return dTongTien;
+        }
+
+        public ActionResult GioHang()
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            if (lstGioHang.Count == 0)
+            {
+                return RedirectToAction("Index", "SachOnline");
+            }
+            ViewBag.TongSoLuong = TongSoLuong();
+            ViewBag.TongTien = TongTien();
+            return View(lstGioHang);
+        }
+
+
+    }
+}
